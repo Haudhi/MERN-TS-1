7ab17b9f334a18b5d2cfddfa6990c699bbec2c08
@haudhiizza <haudhi.izzah@salt.co.id>
2023-06-13 21:15:16 +0700

    adding backend

---
+++ Backend/src/routes/notes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as NotesController from "../controllers/notes"
+
+const router = express.Router()
+
+router.get("/", NotesController.getNotes);
+router.get("/:noteId", NotesController.getNote);
+router.post("/", NotesController.createNotes);
+router.patch("/:noteId", NotesController.updateNote);
+router.delete("/:noteId", NotesController.deleteNote);
+
+export default router;
